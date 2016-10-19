56d392f0b7717fcfdc018f7df9bf21648174b255
OMGaPineapple <laws0056@algonquinlive.com>
2016-10-19 13:25:16 -0400

    Added PageBase

---
+++ App_Code/PresentationLayer/PageBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PageBase
+/// </summary>
+public class PageBase
+{
+    public PageBase()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
