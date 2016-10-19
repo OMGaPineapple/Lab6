efed24d86e077947f9322917d9551b1f39780533
OMGaPineapple <laws0056@algonquinlive.com>
2016-10-19 15:09:01 -0400

    Implemented the add Course Functionality and form

--- AddCourse.aspx.cs
+++ AddCourse.aspx.cs
@@ -7,8 +7,59 @@ using System.Web.UI.WebControls;
 
 public partial class AddCourse : PageBase
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    Course userCourse = null;
+    CourseComparer sortBy = new CourseComparer();
 
+    protected void Page_PreRender(object sender, EventArgs e)
+    {
+        List<Course> userCourse = CourseDataAccess.retreiveAllCourses();
+        userCourse.Sort(sortBy);
+
+        if (userCourse.Count == 0)
+        {
+            TableRow lastRow = new TableRow();
+            TableCell lastRowCell = new TableCell();
+            lastRowCell.Text = "There are currently no students enlisted";
+            lastRowCell.ForeColor = System.Drawing.Color.Red;
+            lastRowCell.ColumnSpan = 4;
+            lastRowCell.HorizontalAlign = HorizontalAlign.Center;
+            lastRow.Cells.Add(lastRowCell);
+            tblCourses.Rows.Add(lastRow);
+        }
+        else
+        {
+
+            foreach (Course course in userCourse)
+            {
+                {
+                    TableRow row = new TableRow();
+
+                    TableCell cell = new TableCell();
+                    cell.Text = course.CourseNumber;
+                    row.Cells.Add(cell);
+
+                    cell = new TableCell();
+                    cell.Text = course.CourseName;
+                    row.Cells.Add(cell);
+
+                    cell = new TableCell();
+                    cell.Text = course.WeeklyHours.ToString();
+                    row.Cells.Add(cell);
+
+                    tblCourses.Rows.Add(row);
+                }
+            }
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string courseNumber = txtCourseNumber.Text;
+        string courseName = txtCourseName.Text;
+        string stringCourseHours = txtCourseHours.Text;
+        int courseHours = Int32.Parse(stringCourseHours);
+
+        userCourse = new Course(courseNumber, courseName, courseHours);
+        CourseDataAccess.addNewCourse(userCourse);
     }
 }
---
+++ App_Code/BuisnessEntities/CourseComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CourseComparer
+/// </summary>
+public class CourseComparer : IComparer<Course>
+{
+    public int Compare(Course course1, Course course2)
+    {
+        return course1.CourseName.CompareTo(course2.CourseName);
+    }
+}
